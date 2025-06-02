79b21684439ca49932c17cff62a4ef7c07274322
Documenter.jl <documenter@juliadocs.github.io>
2025-06-02 18:21:36 +0000

    build based on 8388de1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MapMaskZz","page":"Home","title":"MapMaskZz","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MapMaskZz.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
